8af111ddc8d00b093a859668d0c36d7d78781195
Donal Devine <5518040+donaldevine@users.noreply.github.com>
2022-01-20 21:20:57 +0000

    added authentication

---
+++ ConfArch.Web/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfArch.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserRepository userRepository;
+
+        public AccountController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return View(new LoginModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            
+        }
+    }
+}
